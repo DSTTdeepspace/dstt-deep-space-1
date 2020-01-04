ff827c4fafedeadb883e37940b3c88d2788af89b
Rishit Naskar <59470374+dstt-code@users.noreply.github.com>
2020-01-04 10:30:24 +0530

    Create sin,cos,tan calc.py

---
+++ sin,cos,tan calc.py
@@ -0,0 +1,8 @@
+import math 
+n = int(input("enter a number"))
+print ("The value of sine is : ", end="") 
+print (math.sin(a)) 
+print ("The value of cosine is : ", end="") 
+print (math.cos(a)) 
+print ("The value of tangent is : ",end="")
+print (math.tan(a))
